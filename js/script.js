6fd11e652c931eb02ac6004cdcc73d04a7f3d53b
Azhar2022282 <azharjaya2022@gmail.com>
2025-05-12 20:16:54 +0800

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// Toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
-// ketika menu di klik
-document.querySelector("#menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
